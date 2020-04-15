a93e6e73198972860edaa61b5ca20f400854e8da
Abdul Habra <abdul@habra-invalid.com>
2020-04-15 23:45:08 +0000

    refactor to cleanup and remove duplication

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -10,61 +10,18 @@ import javax.sql.DataSource;
 import java.sql.*;
 import java.util.List;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 public class JdbcTimeEntryRepository implements TimeEntryRepository {
     private JdbcTemplate jdbcTemplate;
 
-    private interface Sql {
-        String insert = "insert into time_entries (project_id, user_id, date, hours) " +
-                "values (?, ?, ?, ?)";
-        String findById = "select id, project_id, user_id, date, hours from time_entries " +
-                "where id = ?";
-        String selectAll = "SELECT id, project_id, user_id, date, hours FROM time_entries";
-        String update = "update time_entries set project_id=?, user_id=?, date=?, hours=? " +
-                "where id = ?";
-        String delete = "delete from time_entries where id=?";
-    }
-
-    private static class TimeEntryRowMapper implements RowMapper<TimeEntry> {
-        @Override
-        public TimeEntry mapRow(ResultSet rs, int rowNum) throws SQLException {
-            TimeEntry timeEntry = new TimeEntry();
-            timeEntry.setId(rs.getLong("id") );
-            timeEntry.setProjectId(rs.getLong("project_id"));
-            timeEntry.setUserId( rs.getLong("user_id") );
-            timeEntry.setDate(rs.getDate("date").toLocalDate());
-            timeEntry.setHours(rs.getInt("hours"));
-            return timeEntry;
-        }
-    }
-
-    private static final TimeEntryRowMapper TIME_ENTRY_ROW_MAPPER = new TimeEntryRowMapper();
-
-    public JdbcTimeEntryRepository(DataSource dataSource) {
+     public JdbcTimeEntryRepository(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    @Override
-    public TimeEntry create(TimeEntry timeEntry) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        PreparedStatementCreator psc = con -> {
-            PreparedStatement statement = con.prepareStatement(Sql.insert, Statement.RETURN_GENERATED_KEYS);
-            statement.setLong(1, timeEntry.getProjectId());
-            statement.setLong(2, timeEntry.getUserId());
-            statement.setDate(3, Date.valueOf( timeEntry.getDate()));
-            statement.setInt(4, timeEntry.getHours());
-            return statement;
-        };
-        jdbcTemplate.update(psc, keyHolder);
-
-        long id = keyHolder.getKey().longValue();
-        return find(id);
-    }
-
-    @Override
+     @Override
     public TimeEntry find(long id) {
-        Object[] args = {id};
-        List<TimeEntry> entries = jdbcTemplate.query(Sql.findById, args, TIME_ENTRY_ROW_MAPPER);
+        List<TimeEntry> entries = read(Sql.findById, id);
         if (entries.isEmpty()) {
             return null;
         }
@@ -74,7 +31,31 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
 
     @Override
     public List<TimeEntry> list() {
-        return jdbcTemplate.query(Sql.selectAll, TIME_ENTRY_ROW_MAPPER);
+        return read(Sql.selectAll);
+    }
+
+    private List<TimeEntry> read(String sql, Object... args) {
+        return jdbcTemplate.query(sql, args, TIME_ENTRY_ROW_MAPPER);
+    }
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        PreparedStatementCreator psCreator = con -> buildInsertStatement(con, timeEntry);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(psCreator, keyHolder);
+
+        //noinspection ConstantConditions
+        return find(keyHolder.getKey().longValue());
+    }
+
+    private static PreparedStatement buildInsertStatement(Connection con, TimeEntry timeEntry) throws SQLException {
+        PreparedStatement ps = con.prepareStatement(Sql.insert, RETURN_GENERATED_KEYS);
+        ps.setLong(1, timeEntry.getProjectId());
+        ps.setLong(2, timeEntry.getUserId());
+        ps.setDate(3, Date.valueOf( timeEntry.getDate()));
+        ps.setInt(4, timeEntry.getHours());
+        return ps;
     }
 
     @Override
@@ -88,5 +69,24 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
     public void delete(long id) {
         jdbcTemplate.update(Sql.delete, id);
     }
+
+    private interface Sql {
+        String insert = "insert into time_entries (project_id, user_id, date, hours) values (?, ?, ?, ?)";
+        String findById = "select id, project_id, user_id, date, hours from time_entries where id = ?";
+        String selectAll = "SELECT id, project_id, user_id, date, hours FROM time_entries";
+        String update = "update time_entries set project_id=?, user_id=?, date=?, hours=? where id = ?";
+        String delete = "delete from time_entries where id=?";
+    }
+
+    private static final RowMapper<TimeEntry> TIME_ENTRY_ROW_MAPPER = (rs, rowNum) -> {
+        TimeEntry timeEntry = new TimeEntry();
+        timeEntry.setId(rs.getLong("id") );
+        timeEntry.setProjectId(rs.getLong("project_id"));
+        timeEntry.setUserId( rs.getLong("user_id") );
+        timeEntry.setDate(rs.getDate("date").toLocalDate());
+        timeEntry.setHours(rs.getInt("hours"));
+        return timeEntry;
+    };
+
 
 }
